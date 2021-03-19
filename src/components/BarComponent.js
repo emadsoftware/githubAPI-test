bd7a8d912c4167a22502c6f8eb270c6809b23aa1
EA <icyrealm@gmail.com>
2021-03-19 01:18:34 -0400

    Some updates

--- src/components/BarComponent.js
+++ src/components/BarComponent.js
@@ -4,6 +4,7 @@ import {Bar} from 'react-chartjs-2';
 // Generate data
 // TODO: Get recent commit number by month https://api.github.com/users/icyrealm/events
 let myArr = [10,4,3,5,6,8,3,5,6,7,8,9];
+let myArr2 = [1,2,3,4,5,6,7,8,9,8,7,6];
 
 // Render data
 const data = {
@@ -18,7 +19,16 @@ const data = {
       hoverBorderColor: 'rgba(255,99,132,1)',
       data: myArr
     }
-  ]
+    // ,{
+    //   label: 'My Commit History',
+    //   backgroundColor: 'rgba(255,99,132,0.2)',
+    //   borderColor: 'rgba(255,99,132,1)',
+    //   borderWidth: 1,
+    //   hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+    //   hoverBorderColor: 'rgba(255,99,132,1)',
+    //   data: myArr2
+    // }
+  ],
 };
 
 export default class BarComponent extends React.Component{
